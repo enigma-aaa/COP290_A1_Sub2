01c964de48ae540e8c99d8c8a4c28de7e88b9b74
Siddhartha Singh <sidsiskilled@gmail.com>
2024-01-27 19:16:48 +0530

    Finally able to get all info

--- COP_290_Assignment1-main/stockInfoEff.py
+++ COP_290_Assignment1-main/stockInfoEff.py
@@ -1,22 +1,37 @@
 import pandas as pd 
 import stockData
+import time
 from concurrent.futures import ThreadPoolExecutor
 df = pd.read_csv('./Data_folder/NSE_Stock_List.csv')
 symbols = df['Symbol']
-
-columnNames = ['industry','industryKey','sector'
-,'sectorKey','sectorDisp','previousClose','open',
+print("symbols are:",symbols)
+columnNames = ['industry','sector','previousClose','open',
 'dayLow','dayHigh','previousClose','currentPrice','volume'
-,'forawrdPE','trailingPE','averageVolume']
-ColumnArr = [ [] for i in range(len(arr))]
+,'trailingPE','averageVolume']
+defaultVal = ['','',0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+ColumnArr = [ [] for i in range(len(columnNames))]
 def get_stats(symbolName):
+    #print("symbolName is:",symbolName)
     info = stockData.getInfo(symbolName)
-    for i in range(len(arr)):
-        elm = arr[i]
-        ColumnArr[i].append(info[elm])
+    for i in range(len(columnNames)):
+        elm = columnNames[i]
+        if elm in info:
+            ColumnArr[i].append(info[elm])
+        else:
+            ColumnArr[i].append(defaultVal[i])
+def getAllInfos():
+    startTime = time.time()
+    #for symbolName in symbols:
+    #    get_stats(symbolName)
+    with ThreadPoolExecutor() as executor:
+        executor.map(get_stats,symbols)
+    endTime = time.time()
+    print("timeElapsed =",endTime-startTime)
 
-with ThreadPoolExecutor() as executor:
-    executor.map(get_stats,ticker_list)
-
-df = pd.DataFrame(data = ColumnArr,index = symbols,columns = columnNames)
-df.to_pickle('./Data_folder/AllStocks.pkl')
+    dataFrameDict = {}
+    for i in range(len(columnNames)):
+        dataFrameDict[columnNames[i]] = ColumnArr[i]
+    df = pd.DataFrame(dataFrameDict,index = symbols)
+    #df = pd.DataFrame(data = ColumnArr,index = symbols,columns = columnNames)
+    df.to_pickle('./Data_folder/AllStocks.pkl')
+    df.to_csv('./Data_folder/AllStocksTemp.csv')
