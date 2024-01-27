a84555d0b0326aea3cd25f7d54551a84c84b8922
Siddhartha Singh <sidsiskilled@gmail.com>
2024-01-27 17:49:11 +0530

    Made modification to gitignore so that it doesn't track the data of stocks wea re putting in

--- COP_290_Assignment1-main/stockInfoEff.py
+++ COP_290_Assignment1-main/stockInfoEff.py
@@ -1,5 +1,22 @@
 import pandas as pd 
+import stockData
 from concurrent.futures import ThreadPoolExecutor
 df = pd.read_csv('./Data_folder/NSE_Stock_List.csv')
 symbols = df['Symbol']
-print(symbols)
+
+columnNames = ['industry','industryKey','sector'
+,'sectorKey','sectorDisp','previousClose','open',
+'dayLow','dayHigh','previousClose','currentPrice','volume'
+,'forawrdPE','trailingPE','averageVolume']
+ColumnArr = [ [] for i in range(len(arr))]
+def get_stats(symbolName):
+    info = stockData.getInfo(symbolName)
+    for i in range(len(arr)):
+        elm = arr[i]
+        ColumnArr[i].append(info[elm])
+
+with ThreadPoolExecutor() as executor:
+    executor.map(get_stats,ticker_list)
+
+df = pd.DataFrame(data = ColumnArr,index = symbols,columns = columnNames)
+df.to_pickle('./Data_folder/AllStocks.pkl')
